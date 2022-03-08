3eb87f1d692d4384b887c46a685df23d389c8491
Jin <53550425+jchris27@users.noreply.github.com>
2022-03-08 08:01:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+
+import random
+
+coin = random.randint(0, 1)
+
+if coin == 0:
+  print("Tails")
+else:
+  print("Heads")
+
+print(coin)
+
+
+
+
+
+
+
+
+
+
